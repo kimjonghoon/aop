5d4e30a69bf561cb2023deb9037e5f5ae0dcc0fd
Jonghoon Kim <kim0070@gmail.com>
2020-12-24 14:00:54 +0900

    BankConfig.java fixed

--- src/main/java/net/java_school/bank/BankConfig.java
+++ src/main/java/net/java_school/bank/BankConfig.java
@@ -16,21 +16,21 @@ public class BankConfig {
 	}
 
 	@Bean
-	public BankDao shinhanBankDao() {
+	public BankDao myBankDao() {
 		return new MyBankDao();
 	}
 
 	@Bean
-	public Bank shinhanBank() {
+	public Bank myBank() {
 		Bank bank = new MyBank();
-		bank.setDao(shinhanBankDao());
+		bank.setDao(myBankDao());
 		return bank;
 	}
 
 	@Bean
 	public BankUi bankUi() {
 		BankUi ui = new BankUi();
-		ui.setBank(shinhanBank());
+		ui.setBank(myBank());
 		ui.setStream(System.out);
 		return ui;
 	}
